46a9a013dd8e633d7f8f34bd5adf1bf769dbbaee
h3x <hhhtylerw@protonmail.com>
2025-03-19 19:48:28 -0700

    update config stuff

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Cathedral Research Group';
+export const SITE_DESCRIPTION = 'There are cathedrals everywhere for those with the eyes to see.';
