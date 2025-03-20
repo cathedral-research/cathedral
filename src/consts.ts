92b40860728f95fba53b2ba62a386a5c5a4d08cd
h3x <hhhtylerw@protonmail.com>
2025-03-20 00:01:39 -0700

    shorter desc

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Cathedral Research Group';
-export const SITE_DESCRIPTION = 'There are cathedrals everywhere for those with the eyes to see.';
+export const SITE_DESCRIPTION = 'For those with the eyes to see.';
 export const AUTHORS = ['ignore', 'test', 'test1', 'tyler', 'luke', 'jay'] as const;
